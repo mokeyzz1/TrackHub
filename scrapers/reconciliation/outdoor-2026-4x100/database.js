e7f44453d6220a812e1ec3329f1cde2f67df3fb9
Moses Koroma <174388353+mokeyzz1@users.noreply.github.com>
2026-09-03 03:00:13 -0400

    Include legacy review rows in private recheck

--- scrapers/reconciliation/outdoor-2026-4x100/database.js
+++ scrapers/reconciliation/outdoor-2026-4x100/database.js
@@ -242,7 +242,7 @@ class ReconciliationDatabase {
         WHERE scope_key = $1
           AND ($2::integer IS NULL OR meet_id = $2)
           AND status = 'needs_review'
-          AND source_candidates #>> '{${RECONCILIATION_KEY},queue_state}' = 'finished'
+          AND COALESCE(source_candidates #>> '{${RECONCILIATION_KEY},queue_state}', 'finished') = 'finished'
           AND (
             source_candidates #>> '{${RECONCILIATION_KEY},source_url}' IS NOT NULL
             OR source_candidates #>> '{${RECONCILIATION_KEY},tfrrs_candidate,url}' IS NOT NULL
